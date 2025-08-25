91a4b407ede1a5c5773c6bc4d7c77ef2a6eec983
tikrack <tikrackcode@gmail.com>
2025-08-25 23:44:34 +0330

    [First] -> add first version

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const elm_boxs = document.querySelectorAll(".box")
+
+  elm_boxs.forEach(function(item) {
+      let port = item.children[0]
+      let edit_btn = item.children[1]
+
+      edit_btn.addEventListener("click", function(e) {
+        port.style.display = "none"
+        item.insertAdjacentHTML("beforeend", `
+          <input type="number" class="edit-input" value="${port.innerText}"/>
+          <button type="button" class="save-button">ثبت</button>
+        `)
+
+          const elm_save_buttons = document.querySelectorAll(".save-button")
+
+          elm_save_buttons.forEach(function(item) {
+            item.addEventListener("click", function() {
+              item.previousElementSibling.previousElementSibling.previousElementSibling.innerText = item.previousElementSibling.value
+              item.previousElementSibling.previousElementSibling.previousElementSibling.style.display = "inline"
+              item.previousElementSibling.remove()
+              item.remove()
+            })
+          })
+
+      })
+  })
+})
